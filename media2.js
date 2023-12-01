061fb90ee672a29e0ca8ad059aaa77a40cd9f33b
bmelovaz <biamelovaz@gmail.com>
2023-12-01 10:34:22 -0300

    Verificar de os números estão dentro do range

--- media2.js
+++ media2.js
@@ -1,3 +1,4 @@
+// Algoritmo nota do Aluno
 const readline = require('readline')
 
 const rl = readline.createInterface({
@@ -5,8 +6,11 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-function limiteNota(a,b,c){
-    console.log('Num fora do range!') 
+function VerificaLimiteNota(notaAluno1,notaAluno2,notaAluno3){
+    if ((notaAluno1 < 0 || notaAluno1 > 10) || (notaAluno2 < 0 || notaAluno2 > 10) || (notaAluno3 < 0 || notaAluno3 > 10) ) {
+        console.log('Por gentileza informar números de 0 a 10!')
+        process.exit()
+    }
 }
 
 rl.question('Informe a primeira a nota: ', (N1) => {
@@ -15,7 +19,10 @@ rl.question('Informe a primeira a nota: ', (N1) => {
             
             const nota1 = parseFloat(N1)
             const nota2 = parseFloat(N2)
+
             const nota3 = parseFloat(N3)
+
+            VerificaLimiteNota(nota1,nota2,nota3)
 
             if (!isNaN (nota3) && !isNaN (nota2) && !isNaN (nota3)){
 
